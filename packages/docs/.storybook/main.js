7d717a27fcad00c87679913019e2c6c7ac19ea73
Pedro Rubinger <pedro.rubinger@gmail.com>
2023-06-20 11:52:24 -0300

    chore: adjust repository name on main.js file

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/05-design-system/'
+      config.base = '/ignite-design-system/'
     }
 
     return config
