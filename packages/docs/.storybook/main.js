d61b674521445a30ba3459aa13ae7fc18f752870
Pedro Rubinger <pedro.rubinger@gmail.com>
2023-06-20 11:38:08 -0300

    chore: add ci workflow

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -16,4 +16,11 @@ module.exports = {
   features: {
     storyStoreV7: true,
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/05-design-system/'
+    }
+
+    return config
+  },
 }
